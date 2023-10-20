694b9056b826737d6a6282d98e5c0960dd9a4ae9
daniel rivera <danielrivera@NC-danielrivera-JXWHQ56VF6.local>
2023-10-20 10:42:53 +0200

    disable NAT gw

--- infra-base/terraform/main.tf
+++ infra-base/terraform/main.tf
@@ -10,6 +10,9 @@ module "networking" {
   private_subnets    = ["10.120.0.0/22", "10.120.64.0/22", "10.120.128.0/22"]
   db_subnets         = ["10.120.4.0/22", "10.120.68.0/22", "10.120.132.0/22"]
   public_subnets     = ["10.120.32.0/22","10.120.96.0/22", "10.120.160.0/22"]
+  enable_nat_gw      = false
+  create_nat_gw      = false
+  single_nat_gw      = false
 }
 
 
@@ -28,21 +31,21 @@ module "kms_key_eks" {
 # # EKS Cluster
 # ##########################
 
-# module "eks_cluster" {
-#   source                       = "./modules/eks"
-#   environment                  = var.environment
-#   region                       = var.region
-#   project_name                 = var.project_name
-#   cluster_version              = "1.26"
-#   subnet_ids                   = module.networking.main.private_subnets
-#   retention_control_plane_logs = 7
-#   instance_type_worker_nodes   = var.environment == "develop" ? ["t3.medium"] : ["t3.medium"]
-#   AMI_for_worker_nodes         = "AL2_x86_64"
-#   desired_nodes                = 1
-#   max_instances_node_group     = 1
-#   min_instances_node_group     = 1
-#   private_endpoint_api         = true
-#   public_endpoint_api          = true
-#   kms_arn                      = module.kms_key_eks.kms_arn
-#   userRoleARN                  = "arn:aws:iam::${data.aws_caller_identity.id_account.id}:role/user-mgnt-eks-cluster"
-# }
+module "eks_cluster" {
+  source                       = "./modules/eks"
+  environment                  = var.environment
+  region                       = var.region
+  project_name                 = var.project_name
+  cluster_version              = "1.26"
+  subnet_ids                   = module.networking.main.public_subnets
+  retention_control_plane_logs = 7
+  instance_type_worker_nodes   = var.environment == "develop" ? ["t3.medium"] : ["t3.medium"]
+  AMI_for_worker_nodes         = "AL2_x86_64"
+  desired_nodes                = 1
+  max_instances_node_group     = 1
+  min_instances_node_group     = 1
+  private_endpoint_api         = true
+  public_endpoint_api          = true
+  kms_arn                      = module.kms_key_eks.kms_arn
+  userRoleARN                  = "arn:aws:iam::${data.aws_caller_identity.id_account.id}:role/user-mgnt-eks-cluster"
+}
